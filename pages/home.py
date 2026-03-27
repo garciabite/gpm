fb38cd79e58da0262c9a0d38d25e97e8de3dbbc8
BITe <cristian.sagarcia@gmail.com>
2026-03-27 10:54:07 -0300

    home.py

--- pages/home.py
+++ pages/home.py
@@ -1,69 +1,61 @@
 import streamlit as st
-import time
 from auxiliar.mapa import mapa
 from auxiliar.texto import resumo, aplicacao, texto_mapa, separador
 from auxiliar.lateral import barra
 
+
+# 2>>> Criar o layout da página no Streamlit_______________________________________________________________________________________________________________________
+
+# Parametro st.set_page_config:
+# Configura o título da aba do navegador, define o layout da pagina como "wide" (largura total da tela) e define um ícone para aba.
+
 st.set_page_config(page_title="GPM - Mecânico", layout="wide", page_icon="⚙")
 
 
-# >>> Ocultar barra superior SOMENTE nesta página
-st.markdown("""
-    <style>
-        #MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
-        header {visibility: hidden;}
-    </style>
-""", unsafe_allow_html=True)
-
-# Título
+# Parametro st.markdown:
+# Insere texto em formato HTML. Aqui, está sendo inserido um título grande e centralizado com o texto "GPM - GESTÃO DE PROJETOS MEBRAFE".
+# O parâmetro unsafe_allow_html=True permite o uso de HTML personalizado.
 st.markdown("""
     <h1 style="text-align:center; font-size:40px;">GPM - GESTÃO DE PROJETOS MEBRAFE</h1>
-""", unsafe_allow_html=True)
+    """, unsafe_allow_html=True)
 
 separador()
 
-# Inicializa estado
-if 'slide_index' not in st.session_state:
-    st.session_state.slide_index = 0
+# Parametro st.coluns
+# st.columns(2): Cria duas colunas, dividindo o layout da página em duas partes.
+# with coluna1: Tudo dentro deste bloco será exibido na primeira coluna. A função st.image insere uma imagem localizada no caminho "assets/fluxo.png".
+#with coluna2:Tudo dentro deste bloco será exibido na segunda coluna. A função resumo() é chamada aqui para exibir o texto relacionado ao resumo, que é importado do módulo texto.
 
-# Placeholder para os slides
-slide_container = st.empty()
+coluna1, coluna2 = st.columns(2)
 
-# Lógica de apresentação de slides
-with slide_container.container():
-    slide = st.session_state.slide_index
+with coluna1:                                                                                                   # Parametro para inserir conteudo na primeira coluna, lado esquerdo.
+    st.image(r"assets/fluxo.png", caption="", width="stretch")                                                  # Parametro para adiconar imagem.
 
-    if slide == 0:
-        col1, col2 = st.columns(2)
-        with col1:
-            st.image("assets/fluxo.png", width="stretch")
-        with col2:
-            resumo()
+with coluna2:                                                                                                   # Parametro para inserir conteudo na segunda coluna, lado direito.
+    resumo()                                                                                                    # Texto com a função resumo.
 
-    elif slide == 1:
-        col3, col4 = st.columns(2)
-        with col3:
-            texto_mapa()
-        with col4:
-            mapa()
+coluna3, coluna4 = st.columns(2)                                                                                # Parametro para criar duas colunas.
 
-    elif slide == 2:
-        col5, col6 = st.columns(2)
-        with col5:
-            st.image("assets/tcm.png", width=600)
-        with col6:
-            aplicacao()
+with coluna3:                                                                                                   # Parametro para inserir conteudo na terceira coluna, lado esquerdo.
+    texto_mapa()                                                                                                # Exibe uma seção de texto relacionado ao mapa.
 
-# A parte que faz ele "rodar"
-time.sleep(10)  # Espera 10 segundos
-st.session_state.slide_index = (st.session_state.slide_index + 1) % 3
-st.rerun()
+with coluna4:                                                                                                   # Parametro para inserir conteudo na quarta coluna, lado direito.
+    mapa()                                                                                                      # Exibe o mapa em 3D na segunda coluna.
 
-# Elementos fixos
-separador()
-st.image("assets/simulacao.gif", width="stretch")
-barra()
+separador()                                                                                                     # Chama a função que cria uma linha de separação na página.
 
+coluna5, coluna6 = st.columns(2)                                                                                # Parametro para criar duas colunas
+
+with coluna5:
+    st.image(r"assets/tcm.png", caption="", width=600)                                                          # Parametro para adiconar imagem com largura fixa.
+
+with coluna6:
+    aplicacao()                                                                                                 # Exibe uma seção de texto relacionada à aplicação, importada do módulo texto
+
+separador()                                                                                                     # Chama a função que cria uma linha de separação na página.
+
+st.image(r"assets/simulacao.gif", caption="", width="stretch")                                                      # Adicona a logomarca.
+
+barra()                                                                                                         # Insere a barra lateral na página, usando a função barra importada do módulo lateral.
 
 
